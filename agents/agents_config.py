b10ac76e1ba5df5c54cbf0f91acc7db58b619a0a
Paula <pvillanunez@gmail.com>
2026-03-23 13:24:08 +0100

    Refactor LLM setup and enhance main system prompt with detailed instructions for AI assistant

--- agents/agents_config.py
+++ agents/agents_config.py
@@ -1,12 +1,33 @@
 from tools.tools import tools
 
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langgraph.prebuilt import ToolNode
+from datetime import datetime
+
+from dotenv import load_dotenv
+load_dotenv()
 
 # LLM setup
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
     temperature=0,
+    max_retries=2,
+    timeout=None
 )
 
 # Bind tools to the LLM
-llm.bind_tools(tools)
+llm_tools = llm.bind_tools(tools)
+
+tool_node = ToolNode(tools)
+
+
+main_system_prompt = f"""
+You are an intelligent AI assistant who answers questions.
+Use the retriever tool available to answer questions about europe context.
+Use the location tool available to get the current location of the user.
+Use the get weather tool available to get the weather of a province code. You can use the get province code tool to get the province code.
+You can make multiple calls if needed.
+If you need to look up some information before asking a follow up question, you are allowed to do that!
+Please always cite the specific parts of the documents you use in your answers.
+Today's date is {datetime.today()}
+"""
